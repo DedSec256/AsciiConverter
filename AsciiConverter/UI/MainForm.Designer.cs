bd8327cbf3c36f2fc12e6d3e965e898d64ad51e6
Alex Berezhnyh <berejnih.alex2011@yandex.ru>
2017-09-25 23:31:10 +0300

    common improves

--- AsciiConverter/UI/MainForm.Designer.cs
+++ AsciiConverter/UI/MainForm.Designer.cs
@@ -73,6 +73,9 @@
             this.Controls.Add(this.saveTextButton);
             this.Controls.Add(this.openImageButton);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "MainForm";
             this.Text = "ASCII Converter v1.0";
             this.ResumeLayout(false);
